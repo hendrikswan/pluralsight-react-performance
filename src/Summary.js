c561ef011917f7ddbfb0129f4fe58ee2ffd61536
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2020-04-02 14:57:41 +0200

    useMemo to reuse calculation

--- src/Summary.js
+++ src/Summary.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import levenshtein from "levenshtein";
 import "./Summary.css";
 import { CornerButton } from "./CornerButton";
@@ -9,21 +9,24 @@ export const Summary = React.memo(
 
     const cards = Object.values(props.cards);
 
+    console.time("calc-distances");
+    const distances = useMemo(() => {
+      const distanceCals = { max: 0, min: 100000 };
+      cards.forEach(currentCard => {
+        cards.forEach(compareCard => {
+          if (compareCard === currentCard) {
+            return;
+          }
+          const distance = levenshtein(currentCard.label, compareCard.label);
 
-    console.time('calc-distances')
-    const distances = { max: 0, min: 100000 };
-    cards.forEach(currentCard => {
-      cards.forEach(compareCard => {
-        if (compareCard === currentCard) {
-          return;
-        }
-        const distance = levenshtein(currentCard.label, compareCard.label);
-
-        distances.max = Math.max(distances.max, distance);
-        distances.min = Math.min(distances.min, distance);
+          distanceCals.max = Math.max(distanceCals.max, distance);
+          distanceCals.min = Math.min(distanceCals.min, distance);
+        });
       });
-    });
-    console.timeEnd('calc-distances')
+      return distanceCals;
+    }, [Object.keys(props.cards).length]);
+
+    console.timeEnd("calc-distances");
 
     return (
       <div className={`Summary Summary-${position}`}>
@@ -31,10 +34,26 @@ export const Summary = React.memo(
         <div>Max difference in labels: {distances.max}</div>
         <div>Min difference in labels: {distances.min}</div>
 
-        <CornerButton setPosition={setPosition} corner="top-right" position={position} />
-        <CornerButton setPosition={setPosition} corner="top-left" position={position} />
-        <CornerButton setPosition={setPosition} corner="bottom-left" position={position} />
-        <CornerButton setPosition={setPosition} corner="bottom-right" position={position} />
+        <CornerButton
+          setPosition={setPosition}
+          corner="top-right"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="top-left"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="bottom-left"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="bottom-right"
+          position={position}
+        />
       </div>
     );
   },
